a318216bafa31507c244ea37b9bcb9ac54a73119
Christophe Rotondo <christopherotondo@macbookair-2.home>
2025-06-02 04:59:28 +0200

    index.astro modifications. Removed the markets section (bundle products and markets on the same category). Modified the tech stack and icons

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'e3e0cbc8440d8d546465b595856a4af20225a0fd', queries,  });
-export default client;
-  
